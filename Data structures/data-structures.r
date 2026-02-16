68bf5d86c0009df6a3b3bb963d1132383d19877a
Ben <benturner27@proton.me>
2026-02-16 16:25:34 +0000

    Added list section, showing basic functions of creating and retrieving data from them

--- Data structures/data-structures.r
+++ Data structures/data-structures.r
@@ -43,3 +43,37 @@ symptoms <- factor(c("SEVERE", "MILD", "MODERATE"),
 #retrieving data on patient symptoms
 symptoms > "MODERATE"
 
+#Lists
+
+#Retrieving all data from the first patient without using a list
+subject_name[1]
+temperature[1]
+flu_status[1]
+gender[1]
+blood[1]
+symptoms[1]
+
+#Retrieving all data from the first patient using a list
+subject1 <- list(fullname = subject_name[1],
+                 temperature = temperature[1],
+                 flu_status = flu_status[1],
+                 gender = gender[1],
+                 blood = blood[1],
+                 symptoms = symptoms[1])
+
+#Calling the list object
+subject1
+
+#retrieving a sub-list from the list object
+subject1[2]
+
+#Retrieving only the value of an index in the list object
+subject1[[2]]
+
+#Alternative way for retrieving the value of an index
+#Better for data continuity
+subject1$temperature
+
+#Retrieving multiple indexes in a list object
+subject1[c("temperature", "flu_status")]
+
