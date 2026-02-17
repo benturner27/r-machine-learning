d051f74457c45d859a74039b7e0fd8d523e88a95
Ben <benturner27@proton.me>
2026-02-17 13:34:04 +0000

    Loaded csv file into project, implemented basic exploratorry techniques, converted type variable to factor

--- Naive bayes/naive-bayes.r
+++ Naive bayes/naive-bayes.r
@@ -1,2 +1,16 @@
 #Naive Bayes
 
+#Exploring and preparing data
+
+#Load csv file into environment
+sms_raw <- read.csv("sms_spam.csv")
+
+#Examine observations and variables using str function
+str(sms_raw)
+
+#Convert type variable into factor
+sms_raw$type <- factor(sms_raw$type)
+
+#Examine type factor using str and table functions
+str(sms_raw$type)
+table(sms_raw$type)
