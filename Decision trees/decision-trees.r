802d63cfb7f7555356e7f98ddca6be0a8779650d
Ben <benturner27@proton.me>
2026-02-24 17:26:24 +0000

    Implemented cav file to environment, explored data provided by using analytical function such as table and summary

--- Decision trees/decision-trees.r
+++ Decision trees/decision-trees.r
@@ -10,4 +10,24 @@
 curve(-x * log2(x) - (1 - x) * log2(1 - x),
       col = "red", xlab = "x", ylab = "entropy", lwd = 4)
 
-#
+#Data Preparation
+
+#Data analysis
+
+#Read csv to file, enabling StringAsFactors parameter to true, since all data is
+#categorical
+credit <- read.csv("credit.csv", stringsAsFactors = TRUE)
+str(credit)
+
+#Exploring trends for loans by analysing bank accounts and savings using table
+#function
+table(credit$checking_balance)
+table(credit$savings_balance)
+
+#Analysing loan duration and loan amount using summary function
+summary(credit$months_loan_duration)
+summary(credit$amount)
+
+#Checking the proportion of applicants have defaulted using table
+table(credit$default)
+
