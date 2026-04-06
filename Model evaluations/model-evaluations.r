6918e0be41569ba3e955c7026498beddfe260205
Ben <benturner27@proton.me>
2026-04-06 15:26:52 +0100

    Added methods of calculating the kappa statistic, including manual methods and importing libraries

--- Model evaluations/model-evaluations.r
+++ Model evaluations/model-evaluations.r
@@ -24,3 +24,32 @@ CrossTable(sms_results$actual_type, sms_results$predict_type)
 (4 + 31) / (152 + 1203 + 4 + 31) #error rate
 1 - 0.9748201 #error rate, same as 1 - accuracy
 
+#Other measures of model performance
+
+#Loading library caret
+library(caret)
+confusionMatrix(sms_results$predict_type,
+                sms_results$actual_type, positive = "spam")
+
+#Calculating kappa from CrossTable figures
+
+#Calculating agreement between predictions and actual figures
+pr_a <- 0.865 + 0.109
+pr_a
+
+#Calculating stimated agreement between predictions and actual figures
+pr_e <- 0.868 * 0.888 + 0.132 * 0.112
+pr_e
+
+#Calculating Kappa
+k <- (pr_a - pr_e) / (1 - pr_e)
+k
+
+#Retrieving kappa using vcd
+library(vcd)
+Kappa(table(sms_results$actual_type, sms_results$predict_type))
+
+#Retrieving kappa using irr
+library(irr)
+kappa2(sms_results[1:2])
+
