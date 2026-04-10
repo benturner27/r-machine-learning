5437cf5a5debaed21306aaaa277ac4cf1456da8f
Ben <benturner27@proton.me>
2026-04-10 18:02:36 +0100

    Implemented a 10-fold cross-validation process in order to maximise limited data for model

--- Model evaluations/model-evaluations.r
+++ Model evaluations/model-evaluations.r
@@ -145,12 +145,41 @@ in_train <- createDataPartition(credit$default, p = 0.75, list = FALSE)
 credit_train <- credit[in_train, ]
 credit_test <- credit[-in_train, ]
 
+#Cross-validation
 
+#Creating k-fold to maximise the amount of data available for the model
+set.seed(123)
+folds <- createFolds(credit$default, k = 10)
+str(folds)
 
+#Creating data for the first fold into the test and train sets
+credit01_test <- credit[folds$Fold01, ]
+credit01_train <- credit[-folds$Fold01, ]
 
+#Automating the 10-fold CV
+library(caret)
+library(C50)
+library(irr)
 
+set.seed(123)
+folds <- createFolds(credit$default, k = 10)
 
+cv_results <- lapply(folds, function(x){
+  credit_train <- credit[-x, ]
+  credit_test <- credit[x, ]
+  credit_model <- C5.0(default ~ ., data = credit_train)
+  credit_pred <- predict(credit_model, credit_test)
+  credit_actual <- credit_test$default
+  kappa <- kappa2(data.frame(credit_actual, credit_pred))$value
+  return(kappa)
+})
 
+#Examining kappa results from 10-fold CV
+str(cv_results)
+
+#Calculating the mean kappa and standard deviation fro 10-fold CV
+mean(unlist(cv_results))
+sd(unlist(cv_results))
 
 
 
