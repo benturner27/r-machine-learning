9236543f9e263f56f171c08a932731a9429a3288
Ben <benturner27@proton.me>
2026-04-19 16:57:22 +0100

    Created a tibble using the training dataset, ready for data prep

--- Advanced data preparation/advanced-data-preparation.r
+++ Advanced data preparation/advanced-data-preparation.r
@@ -1,1 +1,8 @@
 #Advanced Data Preparation
+
+#Table structures
+
+#Creating a tibble using titanic.csv
+library(tibble)
+titanic_csv <- read.csv("titanic_train.csv")
+titanic_tbl <- as_tibble(titanic_csv)
