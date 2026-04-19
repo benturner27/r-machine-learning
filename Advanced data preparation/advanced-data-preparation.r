8b66c9955cba1a854bbdeb4443a84e3b0b59c892
Ben <benturner27@proton.me>
2026-04-19 17:21:13 +0100

    Implemented methods of creating tibbles directly from csv and xlsx file formats

--- Advanced data preparation/advanced-data-preparation.r
+++ Advanced data preparation/advanced-data-preparation.r
@@ -6,3 +6,13 @@
 library(tibble)
 titanic_csv <- read.csv("titanic_train.csv")
 titanic_tbl <- as_tibble(titanic_csv)
+
+#Reading rectangular files
+
+#Creating a tibble directly from csv file format
+library(readr)
+titanic_csv <- read_csv("titanic_train.csv")
+
+#Creating a tibble directly from xlsx file format
+library(readxl)
+titanic_train <- read_excel("titanic_train.xlsx")
