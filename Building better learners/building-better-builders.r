dbefee3341a7743871324dd2f3f03dffe5d40522
Ben <benturner27@proton.me>
2026-04-28 13:31:56 +0100

    Added example of trailing multiple candidate models to be used as a final model, in order to find a compromise between performance and computational cost

--- Building better learners/building-better-builders.r
+++ Building better learners/building-better-builders.r
@@ -15,3 +15,21 @@ table(p, credit$default)
 #Examining the predicion results and it's probabilities on the outcome
 head(predict(m, credit))
 head(predict(m, credit, type = "prob"))
+
+#Tuning customisation
+
+#Using trainControl function to customise tuning process
+ctrl <- trainControl(method = "cv", number = 10, selectionFunction = "oneSE")
+
+#Creating a search grid for hyperparameter tuning using expand.grid function
+grid <- expand.grid(model = "tree",
+                    trials = c(1, 5, 10, 15, 20, 25, 30, 35),
+                    winnow = FALSE)
+
+#Experimenting with models defined in grid to determine best one
+set.seed(300)
+m <- train(default ~ ., data = credit, method = "C5.0",
+           metric = "Kappa",
+           trControl = ctrl,
+           tuneGrid = grid)
+m
