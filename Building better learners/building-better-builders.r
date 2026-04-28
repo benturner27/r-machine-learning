f2e661a39db9df056bc87d9d6b93166629adfeab
Ben <benturner27@proton.me>
2026-04-29 00:16:40 +0100

    Implemented XGBoost to improve on the previous methods in both computational cost and model performance. xgbTree method on caret's train function does not work

--- Building better learners/building-better-builders.r
+++ Building better learners/building-better-builders.r
@@ -137,6 +137,106 @@ m_gbm_c <- train(default ~ ., data = credit, method = "gbm",
                  verbose = FALSE)
 m_gbm_c
 
+#Extreme gradient boosting with XGBoost
+
+#Preparing data and transforming data frame into sparse matrix for compatibility
+credit <- read.csv("credit.csv", stringsAsFactors = TRUE)
+library(Matrix)
+credit_matrix <- sparse.model.matrix(~ . -default, data = credit)
+dim(credit_matrix)
+
+#Examining a sample of the sparse matrix with print
+print(credit_matrix[1:5, 1:15])
+
+#Removing intercept column from sparse matrix because it is not needed for the
+#model being trained
+credit_matrix <- credit_matrix[, -1]
+
+#Splitting sparse matrix into training and testing sets at random
+set.seed(12345)
+train_ids <- sample(1000, 900)
+credit_train <- credit_matrix[train_ids, ]
+credit_test <- credit_matrix[-train_ids, ]
+
+#Verifying training and testing splits
+dim(credit_train)
+dim(credit_test)
+
+#Converting default to be able to evaluate model performance later on
+credit_train_labels <- ifelse(credit[train_ids, c("default")] == "yes", 1, 0)
+credit_test_labels <- ifelse(credit[-train_ids, c("default")] == "yes", 1, 0)
+
+#Training the model using the parameters set above and training sparse matrix
+library(xgboost)
+set.seed(555)
+xgb_credit <- xgboost(x = credit_train,
+                      y = credit_train_labels,
+                      nrounds = 100,
+                      max_depth = 6,
+                      learning_rate = 0.3,
+                      min_child_weight = 1,
+                      min_split_loss = 0,
+                      verbosity = 1,
+                      print_every_n = 10,
+                      subsample = 1)
+
+#Using the trained model to predict on the testing sparse matrix
+prob_default <- predict(xgb_credit, credit_test)
+
+#Converting default probability into a binary outcome
+pred_default <- ifelse(prob_default > 0.50, 1, 0)
+
+#Adding predicted and actual values into a confusion matrix
+table(pred_default, credit_test_labels)
+
+#Retrieving the kappa value from the model's predictions
+library(vcd)
+Kappa(table(pred_default, credit_test_labels))
+
+#Creating a search grid to determine the best hyperparameters for the XGBoost
+#model
+grid_xgb <- expand.grid(eta = c(0.3, 0.4),
+                        max_depth = c(1, 2, 3),
+                        colsample_bytree = c(0.6, 0.8),
+                        subsample = c(0.50, 0.75, 1.00),
+                        nrounds = c(50, 100, 150),
+                        gamma = c(0, 1),
+                        min_child_weight = 1)
+
+#Implementing the control and training experiment to find the best tuned model
+#DOES NOT WORK https://github.com/topepo/caret/issues/1412
+library(caret)
+ctrl <- trainControl(method = "cv", number = 10, selectionFunction = "best")
+credit <- read.csv("credit.csv", stringsAsFactors = TRUE)
+set.seed(300)
+m_xgb <- train(default ~ ., data = credit, method = "xgbTree",
+               trControl = ctrl, tuneGrid = grid_xgb, metric = "Kappa",
+               verbosity = 0)
+m_xgb$bestTune
+#According to the book, best tune is: nrounds = 50, max_depth = 3, eta = 0.4,
+#gamma = 1, colsample_bytree = 0.6, min_child_weight = 1
+#subsample figure is unknown, it's not in the book
+
+#Using max function to retrieve the best kappa value from the experiment
+max(m_xgb$results["Kappa"])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
